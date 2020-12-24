170a614aa23ad42d1666fc899f846e212d28f1e5
cheewr85 <cheewr85@naver.com>
2020-12-24 19:03:57 +0900

    CodeLab 2.1 코딩챌린지 + 홈워크

---
+++ 20년 12월/201224/02.1 HomeWork/HomeWork021/app/src/main/java/techtown/org/hellotoast/SecondActivity.java
@@ -0,0 +1,20 @@
+package techtown.org.hellotoast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.textview_show);
+        textView.setText(message);
+    }
+}
