2c7718bfe8c0c5198d520240c0945e304a200784
cheewr85 <cheewr85@naver.com>
2020-12-28 17:17:26 +0900

    201228 코딩챌린지 + 홈워크

---
+++ 20년 12월/201228/CodingChallenge/CodingChallenge041/app/src/main/java/techtown/org/droidcafe/OrderActivity.java
@@ -0,0 +1,20 @@
+package techtown.org.droidcafe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class OrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+        Intent intent = getIntent();
+        String message = "Order: " + intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.order_textview);
+        textView.setText(message);
+    }
+}
