56e0aa5afdeb99e76c70e74096ff609cf20b04c1
JIMNI_FOX <1229jimin@naver.com>
2021-01-21 00:07:09 +0900

    2021-01-20

---
+++ 2021-1월/2021-01-20/Homework_04_5/app/src/main/java/android/example/homework_04_5/SecondFood.java
@@ -0,0 +1,14 @@
+package android.example.homework_04_5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondFood extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second_food);
+    }
+}
